a132090304d55f4ef0f718b73bccdb1190f63a0c
Jeffrey Wokocha <wokoci@gmail.com>
2021-01-27 17:50:27 +0000

    add DriverSetup interface

---
+++ src/test/java/com/example/config/DriverSetUp.java
@@ -0,0 +1,7 @@
+package com.example.config;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public interface DriverSetUp {
+    public interface RemoteDriverObject(DesiredCapabilities  capabilities);
+}
