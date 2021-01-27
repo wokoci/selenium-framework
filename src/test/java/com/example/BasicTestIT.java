835951743958b412142a519ba02f667ceb99ac30
Jeffrey Wokocha <wokoci@gmail.com>
2021-01-27 08:29:52 +0000

    add RepositoryMap.xml

--- src/test/java/com/example/BasicTestIT.java
+++ src/test/java/com/example/BasicTestIT.java
@@ -1,13 +1,11 @@
 package com.example;
 
+import com.example.config.BaseDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class BasicTestIT extends BaseDriver {
